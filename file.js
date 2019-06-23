062ccbcacfc6ad5fc6880f23d0f035dd79027076
Ayyappa Addala <sd.ayyappaaddala@gmail.com>
2019-06-23 19:19:25 +0530

    This is first commit from first working director

---
+++ file.js
@@ -0,0 +1,15 @@
+/**
+ * Hello Method
+ * ! This is method to display hello count
+ * ? High Priority
+ * TODO: Refactor this method
+ * @param {*} count
+ */
+function executeHello(count) {
+  var loopcount = 0;
+  if (count == loopcount) {
+    return "Hey " + count;
+  }
+}
+
+executeHello(0);
